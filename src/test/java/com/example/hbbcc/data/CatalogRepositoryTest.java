b49585f421f25b007b622c84bcc10984f7f1788b
João Lino <joao.oliveira.lino@gmail.com>
2024-09-17 12:53:35 +0200

    Data access unit test descriptions for supporting catalog service.

---
+++ src/test/java/com/example/hbbcc/data/CatalogRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.hbbcc.data;
+
+import org.junit.jupiter.api.Test;
+
+public class CatalogRepositoryTest {
+
+    ///TODO
+    @Test public void whenSingleFullPriceProductIsSearched_expectSingleFullPriceProductRecord() {}
+
+    ///TODO
+    @Test public void whenMultiFullPriceProductsAreSearched_expectListOfFullPriceProductRecords() {}
+
+    ///TODO
+    @Test public void whenSingleDiscountedProductIsSearched_expectSingleDiscountedProductRecord() {}
+
+    ///TODO
+    @Test public void whenMultiDiscountedProductsAreSearched_expectListOfDiscountedProductRecords() {}
+
+    ///TODO
+    @Test public void whenCombinedFullPriceAndDiscountedProductsAreSearched_expectListOfFullPriceAndDiscountedProductRecords() {}
+
+}
