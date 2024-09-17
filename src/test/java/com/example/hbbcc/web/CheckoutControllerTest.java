9fcc41a9174cf4111632831d348be4896c899ff1
João Lino <joao.oliveira.lino@gmail.com>
2024-09-17 14:01:13 +0200

    Web API unit test description for accessing the catalog service.

---
+++ src/test/java/com/example/hbbcc/web/CheckoutControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.hbbcc.web;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckoutControllerTest {
+
+    ///TODO
+    @Test void whenValidCheckoutRequestReceived_containingValidProductIdList_expectValidResponse_containingPrice() {}
+}
