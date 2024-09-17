1aa22b9de287e5bb608888a689b870ce3e8fc0b1
João Lino <joao.oliveira.lino@gmail.com>
2024-09-17 11:28:59 +0200

    Business logic unit test descriptions.

---
+++ src/test/java/com/example/hbbcc/service/CheckoutServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.hbbcc.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckoutServiceTest {
+
+    ///TODO
+    @Test public void whenSingleFullPriceItemCheckout_expectSingleItemPrice() {}
+
+    ///TODO
+    @Test public void whenMultiFullPriceItemCheckout_expectCombinedPrice() {}
+
+    ///TODO
+    @Test public void whenMaximumFullPriceItemCheckout_expectMaximumCombinedPrice() {}
+
+    ///TODO
+    @Test void whenSingleDiscountItemCheckout_expectSingleDiscountItemPrice() {}
+
+    ///TODO
+    @Test void whenMultiDiscountItemsCheckout_expectCombinedDiscountItemsPrice() {}
+
+    ///TODO
+    @Test void whenMaximumDiscountItemsCheckout_expectMaximumDiscountItemsPrice() {}
+
+    ///TODO
+    @Test void whenSingleFullAndDiscountItemCheckout_expectCombinedSingleFullAndDiscountItemsPrice() {}
+
+    ///TODO
+    @Test void whenMultiSingleFullAndDiscountItemsCheckout_expectCombinedSingleFullAndDiscountItemsPrice() {}
+
+    ///TODO
+    @Test void whenMaximumSingleFullAndDiscountItemsCheckout_expectCombinedSingleFullAndDiscountItemsPrice() {}
+
+}
