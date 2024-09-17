13fbd1b3f36320af233c2156d1a4153fa9409f9e
João Lino <joao.oliveira.lino@gmail.com>
2024-09-17 11:50:15 +0200

    Business logic unit test descriptions for supporting catalog service.

---
+++ src/test/java/com/example/hbbcc/service/CatalogServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.hbbcc.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CatalogServiceTest {
+
+    ///TODO
+    @Test public void whenSingleProductIsRequested_expectSingleProduct() {}
+
+    ///TODO
+    @Test public void whenMultiProductsAreRequested_expectListOfProducts() {}
+
+    ///TODO
+    @Test public void whenSingleDiscountedProductIsRequested_expectSingleDiscountedProduct() {}
+
+    ///TODO
+    @Test public void whenMultiDiscountedProductsAreRequested_expectListOfDiscountedProducts() {}
+
+    ///TODO
+    @Test public void whenCombinedFullAndDiscountedProductsAreRequested_expectListOfFullAndDiscountedProducts() {}
+
+}
